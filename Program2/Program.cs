93b1e5e287ff8554fa20f3b427c250db93e26fee
bougounika <31166561+bougounika@users.noreply.github.com>
2017-08-26 12:22:48 +0400

    Add project files.

---
+++ Program2/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * C# Program to Find the Frequency of the Word ʺtheʺ in a given Sentence
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1;
+            Console.WriteLine("Enter the String : ");
+            s1 = Console.ReadLine();
+            Console.WriteLine(counting.CountStringOccurrences(s1, "the"));
+            Console.ReadLine();
+        }
+    }
+    public static class counting
+    {
+        public static int CountStringOccurrences(string text, string pattern)
+        {
+            int count = 0;
+            int i = 0;
+            while ((i = text.IndexOf(pattern, i)) != -1)
+            {
+                i += pattern.Length;
+                count++;
+            }
+            return count;
+        }
+    }
+}
